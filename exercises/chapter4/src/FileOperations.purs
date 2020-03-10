711068d7dbd61d8881d94bdb5c5e83b50fcb5baa
Enrico Benini <ebenini@mdsol.com>
2020-03-10 17:53:38 +0000

    Implement onlyFiles

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -2,8 +2,9 @@ module FileOperations where
 
 import Prelude
 
-import Data.Path (Path, ls)
+import Data.Path (Path, ls, isDirectory)
 import Data.Array (concatMap, (:))
+import Data.Foldable (foldr)
 
 allFiles :: Path -> Array Path
 allFiles root = root : concatMap allFiles (ls root)
@@ -12,3 +13,9 @@ allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+onlyFiles :: Path -> Array Path
+onlyFiles p = guardIsDirectory (isDirectory p) p
+          where
+          guardIsDirectory true  x = foldr (\a b -> (onlyFiles a) <> b) [] (ls x)
+          guardIsDirectory false x = [x]
