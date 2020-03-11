91499705be7cedc3daaadfbe72b5550f193e27e4
Enrico Benini <ebenini@mdsol.com>
2020-03-11 16:51:33 +0000

    Next: Pattern Matching

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -2,8 +2,9 @@ module FileOperations where
 
 import Prelude
 
-import Data.Path (Path, ls, isDirectory)
+import Data.Path (Path, ls, isDirectory, root, size, filename)
 import Data.Array (concatMap, (:))
+import Data.Maybe (Maybe(..), isJust)
 import Data.Foldable (foldr)
 
 allFiles :: Path -> Array Path
@@ -15,7 +16,26 @@ allFiles' file = file : do
   allFiles' child
 
 onlyFiles :: Path -> Array Path
-onlyFiles p = guardIsDirectory (isDirectory p) p
-          where
-          guardIsDirectory true  x = foldr (\a b -> (onlyFiles a) <> b) [] (ls x)
-          guardIsDirectory false x = [x]
+onlyFiles p = foldr (\p1 p2 -> if (isDirectory p1) then (onlyFiles p1) <> p2 else p1 : p2) [] (ls p)
+
+largestFile :: Path -> Maybe Path
+largestFile p = foldr selectLargerFile Nothing (onlyFiles p)
+              where
+              selectLargerFile f Nothing = Just f
+              selectLargerFile f mf@(Just actualF) = do
+                               actualFSize <- size actualF
+                               currentFSize <- size f
+                               if (actualFSize >= currentFSize)
+                                  then mf
+                                  else Just f
+
+whereIs :: String -> Maybe Path
+whereIs searchTerm = whereIs' searchTerm root Nothing
+
+whereIs' :: String -> Path -> Maybe Path -> Maybe Path
+whereIs' searchTerm p parent
+        | (isDirectory p) == false && searchTerm == (filename p) = parent
+        | (isDirectory p) == false && searchTerm /= (filename p) = Nothing
+        | otherwise = foldr matchFile Nothing (ls p)
+        where
+        matchFile p' m = if (isJust m) then m else whereIs' searchTerm p' (Just p)
